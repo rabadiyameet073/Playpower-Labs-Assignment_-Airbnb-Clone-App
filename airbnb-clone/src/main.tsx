295dc797a57a9393017fa2d7ce4a59d5987100fe
rabadiyameet073 <rabadiyameet073@users.noreply.github.com>
2026-07-17 19:27:10 +0530

    add main.tsx

---
+++ airbnb-clone/src/main.tsx
@@ -0,0 +1,16 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles/App.css";
+import "./styles/index.css";
+import App from "./App";
+
+const container = document.getElementById("root");
+if (!container) {
+  throw new Error("Failed to find the root element");
+}
+
+createRoot(container).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
